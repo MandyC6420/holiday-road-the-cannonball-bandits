79677144b9da9c41326e4672bd7544146f57112e
User-42 <a.t.iafrate@gmail.com>
2021-02-03 13:56:12 -0500

    pausing to check pull request; no significant changes

--- scripts/eateries/eatery.js
+++ scripts/eateries/eatery.js
@@ -1,15 +1,32 @@
+import { getEateries, useEateries} from "./EateryProvider.js"
+
+
 //Creates the HTML to be rendered in eatery div when a restaurant is selected. 
 //the eateryObject will end up being used on the eatery object in EaterySelect.js after the change value is matched with the eatery object with the same business name
 
+let detailsContainer = document.querySelector(".modal-body")
+
 export const eateryCard = (eateryObject) => {
 
+   
     return `
     
     <h2>${eateryObject.businessName}</h2>
-    <h3>${eateryObject.city},${eateryObject.state}</h3>
+    <h4>${eateryObject.city},${eateryObject.state}</h4>
     <img class="eatery-image" src="../images/restaurant-clipart.jpg" alt="drawing-of-restaurant">
     <button>Details</button>
 
-    `
+   `
 
+
+}
+
+getEateries()
+let eateries = useEateries()
+for(const eatery of eateries){
+    if(eatery.businessName === eateryObject.businessName){
+        detailsContainer.innerHTML = `
+        <h1>info here</h1>
+        `
+    }
 }
