680747a2a27304547dd69c0e35701ad58dd65051
User-42 <a.t.iafrate@gmail.com>
2021-02-03 16:04:35 -0500

    made modal/pop-up functional

--- scripts/eateries/EaterySelect.js
+++ scripts/eateries/EaterySelect.js
@@ -51,16 +51,14 @@ const eventHub = document.querySelector(".eateries-Dropdown")
 eventHub.addEventListener("change", changeEvent => {
     //console log when the dropdown item is changed; tests to see if listener is detecting change as expected
     console.log("you selected an item from", changeEvent.target.value)
-   //declares a variable for the array produced by useEateries
-   //for-loops through the array
-   //creates a conditional to evaluate the business' name against the changeEvent's value
-   //if they match, the eateryCard function that creates the HTML is inserted into the eateryList div
+
     
    let eateries = useEateries()
     for(const eatery of eateries){
         if(eatery.businessName === changeEvent.target.value){
             document.querySelector(".eateryList").innerHTML = eateryCard(eatery)
         }
+        
     }
    
 
--- scripts/eateries/eatery.js
+++ scripts/eateries/eatery.js
@@ -1,6 +1,7 @@
 import { getEateries, useEateries} from "./EateryProvider.js"
 
 
+
 //Creates the HTML to be rendered in eatery div when a restaurant is selected. 
 //the eateryObject will end up being used on the eatery object in EaterySelect.js after the change value is matched with the eatery object with the same business name
 
@@ -10,6 +11,12 @@ let detailsContainer = document.querySelector(".modal-body")
 
 export const eateryCard = (eateryObject) => {
 
+   console.log("This should be", eateryObject)
+   console.log(eateryObject.description)
+   detailsContainer.innerHTML = `
+   <h2>Description:${eateryObject.description}</h2>
+   `
+
    
 
     return `
@@ -17,14 +24,18 @@ export const eateryCard = (eateryObject) => {
     <h2>${eateryObject.businessName}</h2>
     <h4>${eateryObject.city},${eateryObject.state}</h4>
     <img class="eatery-image" src="../images/restaurant-clipart.jpg" alt="drawing-of-restaurant">
-    <button>Details</button>
+    <button type="button" class="details-button" data-toggle="modal" data-target="#myModal">Details</button>
 
    `
 
  
-    
    
   
 }
 
 
+
+
+      
+         
+  
