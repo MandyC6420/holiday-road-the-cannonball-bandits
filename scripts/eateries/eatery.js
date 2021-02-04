42816beedadb48e965868d740002db0d5cd9e831
User-42 <a.t.iafrate@gmail.com>
2021-02-04 09:50:02 -0500

    added bootstrap to eatery card

--- scripts/eateries/eatery.js
+++ scripts/eateries/eatery.js
@@ -5,6 +5,8 @@ import { getEateries, useEateries} from "./EateryProvider.js"
 //Creates the HTML to be rendered in eatery div when a restaurant is selected. 
 //the eateryObject will end up being used on the eatery object in EaterySelect.js after the change value is matched with the eatery object with the same business name
 
+
+//query selector that targets the modal box
 let detailsContainer = document.querySelector(".modal-body")
 
 
@@ -13,6 +15,7 @@ export const eateryCard = (eateryObject) => {
 
    console.log("This should be", eateryObject)
    console.log(eateryObject.description)
+  //targets modal box. whatever you want in your modal box goes here. 
    detailsContainer.innerHTML = `
    <h2>Description:${eateryObject.description}</h2>
    `
@@ -21,12 +24,18 @@ export const eateryCard = (eateryObject) => {
 
     return `
     
-    <h2>${eateryObject.businessName}</h2>
-    <h4>${eateryObject.city},${eateryObject.state}</h4>
-    <img class="eatery-image" src="../images/restaurant-clipart.jpg" alt="drawing-of-restaurant">
-    <button type="button" class="details-button" data-toggle="modal" data-target="#myModal">Details</button>
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h4 class="card-title">${eateryObject.businessName}</h4>
+    <h5>${eateryObject.city},${eateryObject.state}</h5>
+    <img class="eatery-image" src="../images/restaurant-clipart.jpg" alt="drawing-of-restaurant"><br>
+    <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Details</a>
+   </div>
+   </div> 
+
 
    `
+   //<a href=... holds the element identifiers that make it a pop-up (ex. data-toggle)
 
  
    
