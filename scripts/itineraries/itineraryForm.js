1f702fdf276655cb85a2b6b14164c08ba8cbfa30
User-42 <a.t.iafrate@gmail.com>
2021-02-05 17:03:46 -0500

    refactored save itinerary with .find instead for loops

--- scripts/itineraries/itineraryForm.js
+++ scripts/itineraries/itineraryForm.js
@@ -2,7 +2,7 @@
 import { saveSelections } from "./itineraryProvider.js";
 import { itineraryList } from "./intineraryList.js";
 import { getEateries, useEateries } from "../eateries/EateryProvider.js";
-import { eateryCard } from "../eateries/eatery.js"
+//import { eateryCard } from "../eateries/eatery.js"
 
 
 const saveButtonTarget = document.querySelector(".save-itinerary-container")
@@ -22,26 +22,59 @@ const eventHub = document.querySelector(".save-itinerary-container")
 
 eventHub.addEventListener("click", clickEvent => {
     if(clickEvent.target.id === "selection-save") {
-       console.log("You clicked", clickEvent)
-       let eaterySelection = document.getElementById("selected-eatery").innerText;
-       let eateries = useEateries()
-       for(const eatery of eateries){
-        if(eatery.businessName === eaterySelection){
-            console.log(eatery)
-            const newItinerary = {
-                "eatery": eatery,
-                "attraction": '',
-                "park" : ''
-            }
-            console.log(newItinerary)
-          
-            saveSelections(newItinerary)
-            
-        }
-       
-    }
+        //-----Pulls the name of the selected eatery/park/attraction out of the printed card----
+        let eaterySelection = document.getElementById("selected-eatery").innerText;
+    //  let attractionSelection = document.getElementById("selected-attraction").innerText;
+     // let parkSelection = document.getElementById("selected-park").innerText;
 
-        
+        let eateries = useEateries()
+        let savedEatery = eateries.find(e => e.businessName === eaterySelection)
+
+        //let attractions = useAttractions()
+       //let savedAttraction = attractions.find(a => a.name === attractionSelection)
+
+      //let parks = useParks()
+      //let savedPark =  parks.find(p => p.fullName === parkSelection)
+     
+    
+        console.log(savedEatery)
+        const newItinerary = {
+            "eatery": savedEatery,
+            "attraction": '',
+            "park" : ''
+                    }
+        saveSelections(newItinerary)
+
+
+
+
+    //    console.log("You clicked", clickEvent)
+    //    let eaterySelection = document.getElementById("selected-eatery").innerText;
+    //    let eateries = useEateries()
+    //    for(const eatery of eateries){ //reference select pages for attractions and parks
+    //     if(eatery.businessName === eaterySelection){
+    //         // console.log(eatery)
+    //         const newItinerary = {
+    //             "eatery": eatery,
+    //             "attraction": '',
+    //             "park" : ''
+    //         }
+    //         // console.log(newItinerary)
+    //         saveSelections(newItinerary)
+            
+    //     } 
+    // }
+
+
+}
+   
+
+
+})
+
+
+
+// let eatery =useEateries().find(eaterySelection)
 
 
         // let eaterySelection = document.getElementById("selected-eatery").innerText;
@@ -58,9 +91,5 @@ eventHub.addEventListener("click", clickEvent => {
         // saveSelections(newItinerary)
         // // .then(itineraryList)
         
-    }
-   
 
-
-})
 
